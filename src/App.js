818f02b787cdaeb7a0566b41fc84b1af9435e1a2
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-08 23:22:58 -0400

    Completed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
-  return (
-    <div className="App">
+  const counter = useSelector(state => state.counter);
+  const isAuthenticated = useSelector(state => state.isAuthenticated);
+  const dispatch = useDispatch();
 
+  return (
+    <div className='App'>
+      <h1>Counter From Redux State: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+
+      {isAuthenticated ? <h3>If logged in, show</h3> : ''}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
