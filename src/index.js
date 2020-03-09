7125655c6294c1092abdfe4e3da9f86214ee8b6b
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-08 21:55:58 -0400

    Will add reducer.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// Action - describes what you want to do. Actions are functions that returns an object
+// ACTION - describes what you want to do. Actions are functions that returns an object
 const increment = () => {
   return {
     type: 'INCREMENT'
@@ -13,6 +13,16 @@ const decrement = () => {
   return {
     type: 'DECREMENT'
   };
+};
+
+// RECUCER - takes in an initial state and an action, describes how state will change
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
