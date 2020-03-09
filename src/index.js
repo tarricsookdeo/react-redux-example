e724ef0cd875f450b718d7a7f621f9f5aceadcb9
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-08 21:52:10 -0400

    Creates actions.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Action - describes what you want to do. Actions are functions that returns an object
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  };
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
